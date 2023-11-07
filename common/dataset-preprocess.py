df2a2dd752ac520ff460d36d0949f9156a5d204c
ma-mahmudi <ma.mahmudi18@outlook.com>
2023-11-07 15:09:03 +0330

    push

--- common/dataset-preprocess.py
+++ common/dataset-preprocess.py
@@ -1,5 +1,12 @@
 import stow
+import tensorflow as tf
 from configs import ModelConfigs
+from mltu.dataProvider import DataProvider
+from mltu.preprocessors import ImageReader
+from mltu.transformers import ImageResizer, LabelIndexer, LabelPadding
+from mltu.augmentors import RandomBrightness, RandomRotate, RandomErodeDilate
+from keras import layers
+from keras.models import Model
 
 dataset = []
 vocab = set()
@@ -15,3 +22,63 @@ configs = ModelConfigs()
 configs.vocab = "".join(vocab)
 configs.max_text_length = max_len
 configs.save()
+
+data_provider = DataProvider(
+    dataset=dataset,
+    skip_validation=True,
+    batch_size=configs.batch_size,
+    data_preprocessors=[ImageReader()],
+    transformers=[
+        ImageResizer(configs.width, configs.height),
+        LabelIndexer(configs.vocab),
+        LabelPadding(max_word_length=configs.max_text_length, padding_value=len(configs.vocab))
+    ])
+
+train_data_provider, val_data_provider = data_provider.split(split=0.9)
+
+train_data_provider.augmentors = [RandomBrightness(), RandomRotate(), RandomErodeDilate()]
+
+
+def train_model(input_dim, output_dim, activation='leaky_relu', dropout=0.2):
+
+    input_list = layers.Input(shape=input_dim, name="input")
+
+    # normalize images here instead in preprocessing step
+    input = layers.Lambda(lambda x: x / 255)(input_list)
+
+    x1 = residual_block(input, 16, activation=activation, skip_conv=True, strides=1, dropout=dropout)
+
+    x2 = residual_block(x1, 16, activation=activation, skip_conv=True, strides=2, dropout=dropout)
+    x3 = residual_block(x2, 16, activation=activation, skip_conv=False, strides=1, dropout=dropout)
+
+    x4 = residual_block(x3, 32, activation=activation, skip_conv=True, strides=2, dropout=dropout)
+    x5 = residual_block(x4, 32, activation=activation, skip_conv=False, strides=1, dropout=dropout)
+
+    x6 = residual_block(x5, 64, activation=activation, skip_conv=True, strides=2, dropout=dropout)
+    x7 = residual_block(x6, 32, activation=activation, skip_conv=True, strides=1, dropout=dropout)
+
+    x8 = residual_block(x7, 64, activation=activation, skip_conv=True, strides=2, dropout=dropout)
+    x9 = residual_block(x8, 64, activation=activation, skip_conv=False, strides=1, dropout=dropout)
+
+    squeezed = layers.Reshape((x9.shape[-3] * x9.shape[-2], x9.shape[-1]))(x9)
+
+    blstm = layers.Bidirectional(layers.LSTM(128, return_sequences=True))(squeezed)
+    blstm = layers.Dropout(dropout)(blstm)
+
+    output = layers.Dense(output_dim + 1, activation='softmax', name="output")(blstm)
+
+    train = Model(inputs=input_list, outputs=output)
+
+    return train
+
+
+model = train_model(
+    input_dim=(configs.height, configs.width, 3),
+    output_dim=len(configs.vocab),
+)
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=configs.learning_rate),
+    loss=CTCloss(),
+    metrics=[CWERMetric()],
+)
